723a7484f6b924bb9172c76d3ad62dcbf3228d48
mariasornmo <mariasornmo@gmail.com>
2024-10-31 10:16:02 +0100

    Lagt till hello universe

--- src/exercise13/HelloWorld.java
+++ src/exercise13/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("And hello universe!");
+
+
     }
 }
